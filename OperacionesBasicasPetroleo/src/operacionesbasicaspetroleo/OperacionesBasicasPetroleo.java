b8f8895c3930a0fb0865091cddc3e1f35a751c9d
HoracioHLeon <hhlp.epo@gmail.com>
2020-05-28 14:45:54 -0500

    Humberto Leon

---
+++ OperacionesBasicasPetroleo/src/operacionesbasicaspetroleo/OperacionesBasicasPetroleo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionesbasicaspetroleo;
+
+/**
+ *
+ * @author HoracioH
+ */
+public class OperacionesBasicasPetroleo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //INSTACIAMOS LA CLASE Panel para ejecutarlo
+        Panel Jfrm_panel = new Panel(); 
+        Jfrm_panel.setVisible(true); // Visualizamos el objeto en modo true
+    }
+    
+}
